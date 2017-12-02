4a59fa9547d26afa7542acbb196ab76995374124
HalmonLui <halmonlui@gmail.com>
2017-12-02 14:48:38 -0500

    Parrot goes echo
    echo echo echo echo

---
+++ receive_sms.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    body = request.values.get('Body', None)
+    resp = MessagingResponse()
+    resp.message(body)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
