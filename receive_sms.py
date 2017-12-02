cddc50d6f281043b091164d4a5b84ec349e6ab81
HalmonLui <halmonlui@gmail.com>
2017-12-02 16:00:39 -0500

    Check Command + Echo Works
    Echo Works, created check command function

--- receive_sms.py
+++ receive_sms.py
@@ -5,12 +5,26 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
+
+
 def sms_reply():
     body = request.values.get('Body', None)
+    check = command_check()
     resp = MessagingResponse()
-    resp.message(body)
+    if check == True:
+        body = body[5:]
+        resp.message(body)
+        return str(resp)
 
-    return str(resp)
+def command_check():
+    body = request.values.get('Body', None)
+    body = body.lower()
+    check = body[:5]
+    if check == '!echo':
+        return True
+    else:
+        return False
+
 
 if __name__ == "__main__":
     app.run(debug=True)
